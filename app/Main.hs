466a8b42dcd20d46c1563cfbbfefa186dfbdd4f2
Harry Askham <harryaskham@gmail.com>
2019-06-08 09:22:04 +0100

    Basic waves working

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
-import Lib
+import Csound.Base
+
+bpm = 140
+
+bps = bpm / 60
+
+sin4 = osc bps
+
+sin8 = osc $ 2 * bps
+
+sin1 = osc $ bps / 4
+
+drums = sin8 * tri 110
 
 main :: IO ()
-main = someFunc
+main = dac $ drums
