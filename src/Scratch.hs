35b55acd962894951add7d0116f73f44b17fa17b
Harry Askham <askham@google.com>
2019-06-08 16:43:16 +0100

    Basic tutorial song

---
+++ src/Scratch.hs
@@ -0,0 +1,9 @@
+module Scratch where
+
+import Csound.Base
+
+kick = osc (100 * linloop [1, 0.1, 0, 0.9, 0])
+snare = at (hp 500 23) $ mul (sqrSeq [0, 0, 1, 0, 0, 0, 0.5, 0.2] 4) pink
+hiHat = at (mlp 2500 0.1) $ mul (sawSeq [1, 0.5, 0.2, 0.5, 1, 0, 0, 0.5] 4) white
+ticks = mul (sqrSeq [0, 0, 0, 0, 1, 1] 8) $ osc 440
+song = mul 0.3 $ return (mul 2.4 kick) + return Scratch.ticks + snare + hiHat
