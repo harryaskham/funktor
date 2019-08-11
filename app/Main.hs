b1bb11d58fa921a73df8bf938487153d8b336634
Harry Askham <harryaskham@gmail.com>
2019-08-11 17:06:57 +0100

    Adds back in the imports in main

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,14 @@
 module Main where
 
+import Csound.Base
+import Csound.Patch
+import Csound.Sam
+import Csound.Sam.Core
+import Tabs
+import Note
+import Melody
 import FirstSong
 import Tools
 
 main :: IO ()
-main = runToDisk minSong
+main = runToDisk houseSong
