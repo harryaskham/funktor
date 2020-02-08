ea5211983bdf51205de60e4eafd71dd4f375b7a8
Harry Askham <harryaskham@gmail.com>
2020-02-08 10:05:49 +0000

    Introduce lenses and compilation environments

--- src/Saturday.hs
+++ src/Saturday.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Saturday where
 
 import Csound.Base hiding (Tab, clp, Duration)
+import Csound.Sam
 import Csound.Patch
 import Tabs
 import Tools
@@ -16,7 +19,19 @@ import Data.Ord
 
 bpm = 140
 
-arp = (compileTrack bpm epiano1 $ (toMel . repeatToBeats 16) (sort $ expandScale [6, 7, 8, 9] (minorChord C) ?? 0.3 ?? 0.5)) * (fromMono $ sinEnv bpm 0 (beats 1))
+data Env = Env { _bpm :: Bpm
+               , _patch :: Patch2
+               , _beatDuration :: Sig
+               }
+makeLenses ''Env
+
+compileWith :: Env -> [Pch] -> Sig2
+compileWith env notes = compileTrack (env^.bpm) (env^.patch) (toMel . repeatToBeats (env^.beatDuration) $ notes)
+
+arpNotes = sort $ expandScale [6, 7, 8, 9] (minorChord C) ?? 0.3 ?? 0.5
+arpEnv = Env bpm epiano2 16
+arpEnvelope = fromMono $ sinEnv bpm 0 (beats 1)
+arp = (compileWith arpEnv arpNotes) * arpEnvelope
 
 intro = arp
 
