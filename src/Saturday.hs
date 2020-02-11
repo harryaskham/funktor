5fd9c2da183509ac8ae4346b8fa28601c3a92d72
Harry Askham <askham@google.com>
2020-02-11 10:07:39 +0000

    Moves into SE monad

--- src/Saturday.hs
+++ src/Saturday.hs
@@ -55,17 +55,21 @@ qujs = compileD $ DrumTab "_ _ o _ _ _ _ _|_ _ o _ _ _ _ _|_ _ o _ _ _ _ _|_ _ o
 padNotes = Pch <$> (take 4 . cycle $ minorChord C) ?? 5 ?? 0.3 ?? 8
 pad = compileWith (Env gBPM razorPad $ fromIntegral numBeats) padNotes
 
-lead = compileWith (Env gBPM polySynth 32) (take 64 $ cycle $ [Pch C 6 0.4 0.5, Silent 0.5])
+lead = compileWith (Env gBPM polySynth $ fromIntegral numBeats) (take 64 $ cycle [Pch C 6 0.4 0.5, Silent 0.5])
 
--- TODO: do all this inside the SE monad
-intro1 = toSeg <$> sum [kick, cows]
-intro2 = toSeg <$> sum [kick, cows, snar]
-maindrum = toSeg <$> sum [kick, cows, snar, mars, tams, sna2]
-mainlead :: SE (Seg Sig2)
-mainlead = toSeg <$> pure lead
-mainpad :: SE (Seg Sig2)
-mainpad = toSeg <$> pure pad
--- all = sum [kick, cows, snar, sna2, cyms, cymt, tams, gros, mars, qujs, pure pad]
+-- drums = sum [kick, cows, snar, sna2, cyms, cymt, tams, gros, mars, qujs, pure pad]
+
+song = do
+  intro1 <- toSeg <$> sum [kick, cows]
+  intro2 <- toSeg <$> sum [kick, cows, snar]
+  maindrum <- toSeg <$> sum [kick, cows, snar, mars, tams, sna2]
+  let mainlead = toSeg lead
+      mainpad = toSeg pad
+  return
+    $ runSeg . loop . mel
+    $ [ forBeats 16 intro1 =:= forBeats 16 mainpad
+      , forBeats 16 intro2
+      , forBeats 32 maindrum =:= forBeats 32 mainpad ]
 
 -- Adds a drop to the given segment
 -- TODO: Find a way to avoid the explicit delay passing
@@ -78,14 +82,6 @@ withDrop delay dropSeg seg = newSeg +:+ drop
 
 addDrop seg = withDrop 12 <$> drop <*> seg
   where
-    drop = (forBeats 4 . toSeg <$> qujs)
-
-song =
-  runSeg . loop . mel
-  <$> sequence [ (=:=) <$> (forBeats 16 <$> intro1) <*> (forBeats 16 <$> mainpad)
-               , forBeats 16 <$> intro2
-               -- TODO: This last one only plays for one beat then dies.
-               , (=:=) <$> (forBeats 32 <$> maindrum) <*> (forBeats 32 <$> mainlead)
-               ]
+    drop = forBeats 4 . toSeg <$> qujs
 
 sat = runB gBPM song
