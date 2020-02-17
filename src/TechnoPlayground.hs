9230a2d6eade02086d6811b56ea5ac5529a04869
Harry Askham <harryaskham@gmail.com>
2020-02-17 18:11:19 +0000

    nicer drums and bass

--- src/TechnoPlayground.hs
+++ src/TechnoPlayground.hs
@@ -27,13 +27,15 @@ song = do
   kcks1 <- drums "O _ _ _ _ _ _ _|o _ _ _ _ _ _ _|O _ _ _ _ _ _ _|o _ _ _ _ _ _ _|" Tr808.bd2
   snrs1 <- drums "_ _ _ _ _ _ _ _|X _ _ _ _ _ _ _|_ _ _ _ _ _ _ _|X _ _ _ _ _ _ _|" Tr808.sn
   chhs1 <- drums "_ _ _ _ o _ _ _|_ _ _ _ . _ _ _|_ _ _ _ o _ _ _|_ _ _ _ . _ _ _|" Tr808.chh
-  ohhs1 <- drums "_ _ _ _ _ _ _ _|_ _ _ _ _ _ _ _|_ _ _ _ _ _ _ _|_ _ _ _ _ _ _ _|" Tr808.ohh
+  ohhs1 <- drums "o _ _ _ o _ _ _|" Tr808.ohh
   cyms1 <- drums "O _ o _ . _ . _|" Tr808.cym
   clps1 <- drums "_ _ _ _ _ _ _ _|X _ _ _ _ _ _ _|_ _ _ _ _ _ _ _|X _ _ _ _ _ _ _|" Hm.clap
 
   let pat0 = har [kcks1]
       pat1 = har [kcks1, clps1]
-      pat2 = har [kcks1, clps1, cyms1]
+      pat2 = har [kcks1, clps1, chhs1]
+      pat3 = har [kcks1, clps1, ohhs1]
+      pat4 = har [kcks1, clps1, cyms1]
 
   pad <-
     -- TODO: rename to e.g. instr
@@ -42,16 +44,25 @@ song = do
     , Silent 8
     ]
 
+  bass <-
+    compileI fmBass2
+    [ Pch root 6 0.8 (1/2)
+    , Pch (doN 3 succC root) 6 0.8 (1/2)
+    , Pch (predC root) 6 0.8 (1/2)
+    ]
+
   drums <-
     cotraverse mel [ forBeats 16 pat0
                    , forBeats 16 pat1
                    , forBeats 32 pat2
+                   , forBeats 32 pat3
+                   , forBeats 32 pat4
                    ]
 
-  return $ loop drums =:= loop pad
+  return $ loop drums =:= loop pad =:= bass
 
 songEnv = SongEnv { _bpm=140
-                  , _beatLength=512
+                  , _beatLength=1024
                   }
 tec' = runSongM songEnv song
 tec = dac =<< tec'
