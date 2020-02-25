4b6f896ee41ba01db119973373071213d2ca5991
Harry Askham <askham@google.com>
2020-02-25 15:47:32 -0800

    lots more lead

--- src/Garage.hs
+++ src/Garage.hs
@@ -35,37 +35,44 @@ song = do
     ]
   let pad = stereoMap (sqrEnv gBPM 0 (1/8) *) <$> pad'
 
-  lead <-
+  lead1' <-
     compileI sawOrgan
     $ takeIxs [3, 9, 1, 8]
     $ sort
     $ expandScale [7, 8, 9] (minorScale root) ?? 0.2 ?? (1/2)
+  let lead1 = stereoMap (sqrEnv gBPM 0.75 32 *) <$> lead1'
 
   high' <- compileI dreamPad [ Pch root 9 0.6 16 ]
   let high = stereoMap ((sinEnv gBPM 0 4 * sqrEnv gBPM 0 (1/8)) *) <$> high'
+
+  lead2' <-
+    compileI triOrgan
+    $ takeIxs [12, 12, 11, 13, 13, 14, 10, 15]
+    $ sort
+    $ expandScale [8, 9, 10] (minorScale root) ?? 0.3 ?? (1/2)
+  let lead2 = stereoMap (sqrEnv gBPM 0.25 16 *) <$> lead2'
+
+  lead3' <-
+    compileI sqrOrgan
+    $ takeIxs [1, 2, 3, 4, 3, 4, 3, 4]
+    $ sort
+    $ expandScale [8, 9, 10] (minorScale root) ?? 0.2 ?? (1/2)
+  let lead3 = stereoMap (sqrEnv gBPM 0.5 16 *) <$> lead3'
+
+  lead4' <-
+    compileI sawOrgan
+    $ takeIxs [10, 9, 8, 6, 3, 6, 3, 6]
+    $ sort
+    $ expandScale [8, 9, 10] (minorScale root) ?? 0.2 ?? (1/2)
+  let lead4 = stereoMap (sqrEnv gBPM 0 16 *) <$> lead4'
 
   kcks <- drums "X _ _ _|o _ _ _|o _ _ _|o _ _ _|" Tr808.bd2
   chhs <- drums "O . _ .|_ . _ .|. . _ .|_ . _ .|" Tr808.chh
   ohhs <- drums "_ _ o _|_ _ . _|_ _ . _|_ _ . _|" Tr808.ohh
   clps <- drums "_ _ _ _|X _ _ _|_ X _ _|X _ _ _|" Hm.clap
   clls <- drums "X X X X|" Tr808.cl
-    {-
-  segs <-
-    traverse (forBeats 64)
-    $ fmap har
-    [ [kcks, clls]
-    , [kcks, clls, pad]
-    , [kcks, clls, pad, clps, high, chhs, ohhs, lead]
-    , [kcks, clls, clps, chhs, ohhs]
-    , [kcks, clls, pad, clps, high, chhs, ohhs, lead]
-    , [kcks, clls, clps, chhs, ohhs]
-    , [kcks, clls, high]
-    , [kcks]
-    ]
-    -}
 
-  -- return $ mel segs
-  return $ har [kcks, clls, pad, clps, high, chhs, ohhs, lead]
+  return $ har [kcks, clls, pad, clps, high, chhs, ohhs, lead1, lead2, lead3, lead4]
 
 songEnv = SongEnv { _bpm=128
                   , _beatLength=8*64
