c79f8db945ed5d51e91d2b8052374f7de74c0491
Harry Askham <harryaskham@gmail.com>
2020-04-10 17:58:44 +0100

    moresticks

--- src/SticksTwo.hs
+++ src/SticksTwo.hs
@@ -34,7 +34,8 @@ song = do
   cyms <- drums "_ _ _ _|_ _ _ _|_ _ _ _|X _ _ _" Tr808.cym
   clps <- drums "_ _ _ _|O _ _ _|_ _ _ _|o _ _ _" Hm.sn1
 
-  let guitar = loop $ mul 0.9 $ constLim (beatsToSecs $ Beats gBPM 8) $ toSeg $ scaleWav 0 (1.058*speedMod) 1 "samples/SitarGuitar.wav"
+  let guitar = loop $ mul 0.8 $ constLim (beatsToSecs $ Beats gBPM 8) $ toSeg $ scaleWav 0 (1.058*speedMod) 1 "samples/SitarGuitar.wav"
+  let guitar1 = loop $ mul 0.8 $ constLim (beatsToSecs $ Beats gBPM 2) $ toSeg $ scaleWav 0 (1.058*speedMod) 1 "samples/SitarGuitar.wav"
 
   wobble <- do
     i <- compileI razorLead $ Pch <$> [D, A, D, F] <*> pure 9 <*> pure 0.5 <*> pure 4
@@ -46,19 +47,32 @@ song = do
         ns = takeIxs [0, 4, 4, 3, 2, 1, 6, 6, 6, 7, 8, 7, 12, 12, 13, 12] notes
     compileI banyan ns
 
-  return
-    $ mel
-    $ constLim (beatsToSecs $ Beats gBPM 16)
-    <$>
-    [ har [arp, kcks]
-    , har [guitar, arp, kcks]
+  envPlayWith (replicate 10 16)
+    [ har [guitar, arp, kcks]
+    , har [guitar1, arp, kcks]
     , har [guitar, arp, kcks, ohhs]
-    , har [guitar, arp, kcks, ohhs, wobble]
+    , har [guitar1, arp, kcks, ohhs, wobble]
     , har [arp, kcks, ohhs, wobble, chhs]
+    , har [kcks, chhs, ohhs, cyms, clps, guitar, wobble, arp]
+    , har [guitar, arp, kcks]
     , har [kcks, chhs, ohhs, cyms, clps, guitar, wobble, arp]
     , har [guitar, arp, kcks]
     , har [arp]
     ] 
+
+envPlayWith :: (MonadReader SongEnv m) => [Double] -> [Seg Sig2] -> m (Seg Sig2)
+envPlayWith lens sigs = do
+  let total = sum lens
+      dels = 0 : scanl1 (+) lens
+
+  envs <-
+    sequence
+    $ (\(len, del) -> sqrTabEnv [OffFor del, OnFor len, OffFor $ total-len-del])
+    <$> zip lens dels
+  
+  return $ har $ (\(sig, env) -> stereoMap (env*) <$> sig) <$> zip sigs envs
+
+
 songEnv = SongEnv { _bpm=speedMod*128
                   , _beatLength=1024
                   }
